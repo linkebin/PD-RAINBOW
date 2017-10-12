64245ca514285e145afc91764bff6c926e144958
linkebin@yidusoft.net <Ly107951>
2017-10-12 10:00:16 +0800

    修改类名

--- src/main/java/com/yidusoft/PdRainbowApplication.java
+++ src/main/java/com/yidusoft/PdRainbowApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PdFainbowApplication {
+public class PdRainbowApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PdFainbowApplication.class, args);
+		SpringApplication.run(PdRainbowApplication.class, args);
 	}
 }
