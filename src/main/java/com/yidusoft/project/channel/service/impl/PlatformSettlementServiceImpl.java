ee9d83e88e490381554616fdf3d31ae413cda7a8
linkebin@yidusoft.net <Ly107951>
2018-05-09 11:34:57 +0800

    账单自动结算

--- src/main/java/com/yidusoft/project/channel/service/impl/PlatformSettlementServiceImpl.java
+++ src/main/java/com/yidusoft/project/channel/service/impl/PlatformSettlementServiceImpl.java
@@ -1,23 +1,17 @@
 package com.yidusoft.project.channel.service.impl;
 
 import com.github.pagehelper.PageHelper;
+import com.yidusoft.core.AbstractService;
 import com.yidusoft.project.channel.dao.ClearingManageMapper;
 import com.yidusoft.project.channel.dao.PlatformSettlementMapper;
 import com.yidusoft.project.channel.domain.PlatformSettlement;
-import com.yidusoft.project.channel.service.ClearingManageService;
 import com.yidusoft.project.channel.service.PlatformSettlementService;
-import com.yidusoft.core.AbstractService;
-
-import com.yidusoft.utils.CodeHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
-import java.math.BigDecimal;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 
 /**
@@ -34,34 +28,34 @@ public class PlatformSettlementServiceImpl extends AbstractService<PlatformSettl
 
     @Override
     public List<Map<String, Object>> findPlatformSettlementList(Map<String,Object> map) {
-        List<Map<String,Object>> yearMonthList = clearingManageMapper.findYearMonthlyCommissions();
-        for (Map<String,Object> m : yearMonthList) {
-            List<String> uids = clearingManageMapper.findYearMonthLikeIds(m.get("create_time").toString());
-            if (uids.size()>0){
-                clearingManageMapper.updateBatchStatus(uids);
-            }
-            String year = m.get("create_time").toString().split("-")[0];
-
-            String month = m.get("create_time").toString().split("-")[1];
-
-            PlatformSettlement platformSettlement = new PlatformSettlement();
-            platformSettlement.setId(UUID.randomUUID().toString());
-            platformSettlement.setCode(CodeHelper.getCode("ZD"));
-            platformSettlement.setCreateTime(new Date());
-            platformSettlement.setYear(year);
-
-            int M = Integer.parseInt(month)-1;
-            platformSettlement.setMonth(String.valueOf(M));
-            if (M<10){
-                platformSettlement.setMonth("0"+M);
-            }
-
-            platformSettlement.setShouldBe(new BigDecimal(m.get("brokerage").toString()));
-            platformSettlement.setClosed(new BigDecimal("0.0"));
-            platformSettlement.setUnliquidated(new BigDecimal(m.get("brokerage").toString()));
-            platformSettlement.setStatus("1");
-            this.save(platformSettlement);
-        }
+//        List<Map<String,Object>> yearMonthList = clearingManageMapper.findYearMonthlyCommissions();
+//        for (Map<String,Object> m : yearMonthList) {
+//            List<String> uids = clearingManageMapper.findYearMonthLikeIds(m.get("create_time").toString());
+//            if (uids.size()>0){
+//                clearingManageMapper.updateBatchStatus(uids);
+//            }
+//            String year = m.get("create_time").toString().split("-")[0];
+//
+//            String month = m.get("create_time").toString().split("-")[1];
+//
+//            PlatformSettlement platformSettlement = new PlatformSettlement();
+//            platformSettlement.setId(UUID.randomUUID().toString());
+//            platformSettlement.setCode(CodeHelper.getCode("ZD"));
+//            platformSettlement.setCreateTime(new Date());
+//            platformSettlement.setYear(year);
+//
+//            int M = Integer.parseInt(month)-1;
+//            platformSettlement.setMonth(String.valueOf(M));
+//            if (M<10){
+//                platformSettlement.setMonth("0"+M);
+//            }
+//
+//            platformSettlement.setShouldBe(new BigDecimal(m.get("brokerage").toString()));
+//            platformSettlement.setClosed(new BigDecimal("0.0"));
+//            platformSettlement.setUnliquidated(new BigDecimal(m.get("brokerage").toString()));
+//            platformSettlement.setStatus("1");
+//            this.save(platformSettlement);
+//        }
         PageHelper.startPage(Integer.parseInt(map.get("page").toString()), Integer.parseInt(map.get("limit").toString()));
         List<Map<String,Object>> data = platformSettlementMapper.findPlatformSettlementList(map);
 
