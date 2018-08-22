0c4542dc3d5a1b02d284589d8a5dfb0d28f7d8ec
cwj <chenwenjie@yidusoft.net>
2018-08-22 11:32:27 +0800

    漏洞防范改进

--- src/main/java/com/yidusoft/project/Interceptor/XssFilter.java
+++ src/main/java/com/yidusoft/project/Interceptor/XssFilter.java
@@ -1,6 +1,7 @@
 package com.yidusoft.project.Interceptor;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
@@ -15,8 +16,10 @@ import java.util.Enumeration;
  * Role:sql盲注，跨站脚本，跨站点请求伪造防范规则
  */
 @Component
-public class XssFilter implements Filter{
+public class XssFilter implements Filter {
     FilterConfig filterConfig = null;
+    @Value("${server.ip}")
+    private String flag;
 
     @Override
     public void destroy() {
@@ -26,23 +29,23 @@ public class XssFilter implements Filter{
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
                          FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest request1= (HttpServletRequest) request;
-        HttpServletResponse response1= (HttpServletResponse) response;
-        String referer=request1.getHeader("Referer");
+        HttpServletRequest request1 = (HttpServletRequest) request;
+        HttpServletResponse response1 = (HttpServletResponse) response;
+        String referer = request1.getHeader("Referer");
         String path = request1.getServletPath();
-        System.out.println("Referer----");
-        System.out.println(referer);
-        System.out.println(path);
+//        System.out.println("Referer----");
+//        System.out.println(referer);
+//        System.out.println(path);
 
         //跨站点请求伪造，条件为部署项目地址域名或者ip
-        if(referer!=null) {
-            if (referer.indexOf("localhost") == -1) {
+        if (referer != null) {
+            if (referer.indexOf(flag) == -1) {
                 chain.doFilter(null, response);
                 response1.sendRedirect(request1.getContextPath() + "/login");
                 return;
             }
-        }else{
-            if(path.indexOf("/login")==-1){
+        } else {
+            if (path.indexOf("/login") == -1) {
                 chain.doFilter(null, response);
                 response1.sendRedirect(request1.getContextPath() + "/login");
                 return;
@@ -50,43 +53,40 @@ public class XssFilter implements Filter{
         }
 
         Enumeration e = request.getParameterNames();
-        int a=0;
-        System.out.println("xss漏洞日志");
+        int a = 0;
+//        System.out.println("xss漏洞日志");
         while (e.hasMoreElements()) {
             String paramName = (String) e.nextElement();
-            String value2 = new String(request.getParameter(paramName).getBytes("ISO-8859-1"),"gb2312");
-            System.out.println(paramName + "=" + request.getParameter(paramName));
-            if(paramName.equals("t")){
+            String value2 = new String(request.getParameter(paramName).getBytes("ISO-8859-1"), "gb2312");
+//            System.out.println(paramName + "=" + request.getParameter(paramName));
+            if (paramName.equals("t")) {
                 break;
             }
-            if(paramName.equals("image")&&request.getParameter(paramName).toString().indexOf("data:image")!=-1){
+            if (paramName.equals("image") && request.getParameter(paramName).toString().indexOf("data:image") != -1) {
                 break;
             }
 
             //防止拦截请求参数里面的数据格式符号(请勿随意修改顺序)
-            value2=value2.replace("(中国标准时间)", "");
-            value2=value2.replace("GMT+0800", "");
-            value2=value2.replace("(??????)", "");
-            if(value2.indexOf("imageText")!=-1){
-                value2=value2.replace(value2.substring(value2.indexOf("imageText"),value2.length()), "");
+            value2 = value2.replace("(中国标准时间)", "");
+            value2 = value2.replace("GMT+0800", "");
+            value2 = value2.replace("(??????)", "");
+            if (value2.indexOf("imageText") != -1) {
+                value2 = value2.replace(value2.substring(value2.indexOf("imageText"), value2.length()), "");
             }
-            value2=value2.replace("\",\"", "");
-            value2=value2.replace("\":[\"", "");
-            value2=value2.replace("{\"","");
-            value2=value2.replace("\"]}","");
-            value2=value2.replace("\"}","");
-            value2=value2.replace("\":\"", "");
-            value2=value2.replace("\":null,\"", "");
-            value2=value2.replace("\":null", "");
-            value2=value2.replace("\":", "");
-            value2=value2.replace(",\"", "");
+            value2 = value2.replace("\",\"", "");
+            value2 = value2.replace("\":[\"", "");
+            value2 = value2.replace("{\"", "");
+            value2 = value2.replace("\"]}", "");
+            value2 = value2.replace("\"}", "");
+            value2 = value2.replace("\":\"", "");
+            value2 = value2.replace("\":null,\"", "");
+            value2 = value2.replace("\":null", "");
+            value2 = value2.replace("\":", "");
+            value2 = value2.replace(",\"", "");
             System.out.println(value2);
-            a=select(value2);
-            if(a==1){
+            a = select(value2);
+            if (a == 1) {
                 chain.doFilter(null, response);
-            }else{
-
-
             }
         }
 
@@ -99,8 +99,8 @@ public class XssFilter implements Filter{
         this.filterConfig = filterConfig;
     }
 
-    public int select(String value){
-        if(value.indexOf(">")!=-1||value.indexOf("<")!=-1||value.indexOf("+")!=-1||value.indexOf("&")!=-1||value.indexOf(")")!=-1||value.indexOf("(")!=-1||value.indexOf("%")!=-1||value.indexOf(";")!=-1||value.indexOf("\'")!=-1||value.indexOf("\"")!=-1){
+    public int select(String value) {
+        if (value.indexOf(">") != -1 || value.indexOf("<") != -1 || value.indexOf("+") != -1 || value.indexOf("&") != -1 || value.indexOf(")") != -1 || value.indexOf("(") != -1 || value.indexOf("%") != -1 || value.indexOf(";") != -1 || value.indexOf("\'") != -1 || value.indexOf("\"") != -1) {
             return 1;
         }
         return 0;
